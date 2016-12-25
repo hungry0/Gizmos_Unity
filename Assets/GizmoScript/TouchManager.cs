b490e97b901f56438248ad9e15c0d4a76630cc22
hungry <277862831@qq.com>
2016-12-25 12:08:24 +0800

    add project

---
+++ Assets/GizmoScript/TouchManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * 	rotate camera && move the camera 
+ */
+
+[RequireComponent(typeof(Camera))]
+public class TouchManager : MonoBehaviour {
+
+	public Transform m_GO;
+
+	private Camera m_MainCamera;
+
+	private float scrollWheel;
+
+	void Awake()
+	{
+		m_MainCamera = GetComponent<Camera> ();
+	}
+
+	void OnEnable()
+	{
+		Lean.LeanTouch.OnFingerDrag += OnFingerDrag;
+	}
+
+	void OnDisable()
+	{
+		Lean.LeanTouch.OnFingerDrag -= OnFingerDrag;
+	}
+
+	void Update () {
+		scrollWheel = Input.GetAxis ("Mouse ScrollWheel");
+
+		m_MainCamera.transform.position += m_MainCamera.transform.forward * scrollWheel * Time.deltaTime * 1000.0f;
+	}
+
+	void OnFingerDrag(Lean.LeanFinger finger)
+	{
+		m_GO.localRotation *= 
+			Quaternion.Euler (finger.DeltaScreenPosition.y * Time.deltaTime * 10.0f,- finger.DeltaScreenPosition.x * Time.deltaTime * 10.0f,0) ;
+
+		if (ShowGizmosManager.PositionChangeCallback != null) {
+			ShowGizmosManager.PositionChangeCallback ();
+		}
+	}
+}
---
+++ Assets/ShowGizmosManager.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Renderer))]
+public class ShowGizmosManager : MonoBehaviour {
+
+	public GameObject m_GizmosParent;
+
+	public static Action PositionChangeCallback;
+
+	private Vector3[] vertices;
+
+	private List<GameObject> gizmos; 
+
+	void Awake()
+	{
+		vertices = GetComponent<MeshFilter> ().sharedMesh.vertices;
+
+		gizmos = new List<GameObject> ();
+
+		ShowIndexGizmos ();
+
+		for (int i = 0; i < vertices.Length; i++) {
+			gizmos.Add (GameObject.Find(m_GizmosParent.name + "/" + i));
+		}
+
+		PositionChangeCallback += UpdateGizmosPos;
+	}
+		
+	void Update()
+	{
+		RaycastHit hit;
+
+		if (Input.GetMouseButtonDown(0) && Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit)) {
+			Debug.Log (hit.triangleIndex);
+
+			if (hit.triangleIndex >= vertices.Length)
+				return;
+
+			IconManager.SetIcon (gizmos[hit.triangleIndex],IconManager.LabelIcon.Red);
+		}
+	}
+
+	[ContextMenu("HideIndex")]
+	void HideIndexGizmos()
+	{
+		m_GizmosParent.SetActive (false);
+	}
+
+	
+	[ContextMenu("ShowIndex")]
+	void ShowIndexGizmos()
+	{
+		if (m_GizmosParent.transform.childCount != 0) {
+			m_GizmosParent.SetActive (true);
+
+			return;
+		}
+
+		for (int i = 0; i < vertices.Length; i++){ 
+			AddGizmos ("" + i,transform.TransformPoint(vertices[i]));
+		}
+	}
+
+	void AddGizmos(string name,Vector3 position)
+	{
+		GameObject go = Instantiate (Resources.Load("ShowIndexGO") as GameObject);
+
+		go.transform.parent = m_GizmosParent.transform;
+
+		go.name = name;
+
+		go.transform.position = position;
+
+		IconManager.SetIcon (go,IconManager.LabelIcon.Green);
+	}
+
+	void UpdateGizmosPos()
+	{
+		var vertices = GetComponent<MeshFilter> ().sharedMesh.vertices;
+
+		for (int i = 0; i < vertices.Length; i++) {
+			gizmos [i].transform.position = transform.TransformPoint(vertices [i]);
+		}  
+	}
+}
